72d78d030882432d6f952b6364f1d36d3de7da41
wzaghal <wisamzaghal@gmail.com>
2014-06-29 11:24:09 -0400

    implement signup

--- app/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-	'url' : MONGODB_URL=process.env.MONGODB_URL
-
-};
--- server.js
+++ server.js
@@ -1,48 +1,42 @@
-'use strict';
+// server.js
 
-//  setup
-//=========
-var express = require('express');
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
 var mongoose = require('mongoose');
 var passport = require('passport');
-var flash = require('connect-flash');
+var flash 	 = require('connect-flash');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-var configDB 	 = require('./app/config/database.js');
 
-//$k#Hsr9Zi6649i77qr]f!Q0b
-var app = express();
-var port = process.env.PORT || 8080;
+var configDB = require('./config/database.js');
 
-// configuration
-//==============
+// configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+ require('./config/passport')(passport); // pass passport for configuration
 
+// set up our express application
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
 
-app.set('view engine', 'ejs'); //set up ejs for templating
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
-
-
-// passport-config
-//================
+// required for passport
 app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-
-
-//launch
-//======
+// launch ======================================================================
 app.listen(port);
-console.log('Port ' + port + ' is live');
+console.log('The magic happens on port ' + port);
