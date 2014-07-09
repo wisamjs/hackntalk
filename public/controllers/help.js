8318a02fb8012eff03907005438ba6c1f375732b
wzaghal <wisamzaghal@gmail.com>
2014-07-09 15:23:46 -0400

    add more views and controllers

---
+++ public/controllers/help.js
@@ -0,0 +1,6 @@
+'use strict';
+// Login comntroller for our view
+angular.module('MyApp')
+  .controller('HelpCtrl', ['$scope', function($scope) {
+
+  }]);
---
+++ public/controllers/merchants.js
@@ -0,0 +1,6 @@
+'use strict';
+// Login comntroller for our view
+angular.module('MyApp')
+  .controller('MerchantCtrl', ['$scope', function($scope) {
+
+  }]);
